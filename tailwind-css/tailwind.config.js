7d7908fa1e4f18816c39890959a89b8b217e0c1f
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-12-20 20:09:40 -0300

    feat: setup tailwind

---
+++ tailwind-css/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Dentro da pasta src, qualquer aquivo que terminar com .html ou .js
+  content: ['./src/**/*.{html, js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
