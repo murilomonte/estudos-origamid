32ab0aea3e4fb2fad8eb3cd42e03e0668da815e9
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-01-10 07:39:41 -0300

    feat: aula plugin

--- tailwind-css/tailwind.config.js
+++ tailwind-css/tailwind.config.js
@@ -1,10 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+import plugin from 'tailwindcss/plugin';
+
 module.exports = {
   // Dentro da pasta src, qualquer aquivo que terminar com .html ou .js
   content: ['./src/**/*.{html, js}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities, addComponents }) {
+      addUtilities({
+        '.text-shadow': {
+          'text-shadow': '1px 1px 6px currentColor'
+        },
+        '.text-shadow-lg': {
+          'text-shadow': '2px 2px 10px currentColor'
+        }
+      }),
+      addComponents({
+        '.btn-ghost': {
+          'border-radius': '9999px',
+          padding: '.5rem 1rem',
+          opacity: '.2',
+          border: '2px solid #ccc'
+        }
+      })
+    })
+  ],
 }
 
