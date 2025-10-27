86dd1eb6ee0fce93764384b31354896822fcbe39
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2025-10-27 13:53:53 -0300

    feat: desafio checkbox

---
+++ react/08-desafio-checkbox/src/App.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+// Otimize o código do slide anterior
+// Utilizando a array abaixo para mostrar
+// cada checkbox na tela.
+
+const coresArray = ["azul", "roxo", "laranja", "verde", "vermelho", "cinza"];
+
+const App = () => {
+  const [cores, setCores] = React.useState([]);
+
+  function handleChange({ target }) {
+    if (target.checked) {
+      setCores([...cores, target.value]);
+    } else {
+      setCores(cores.filter((cor) => cor !== target.value));
+    }
+  }
+
+  function handleChecked(cor) {
+    return cores.includes(cor);
+  }
+
+  return (
+    <form style={{textTransform: 'capitalize'}}>
+      {coresArray.map((cor) => {
+        return (
+          <label key={cor}>
+            <input
+              type="checkbox"
+              value={cor}
+              checked={handleChecked(cor)}
+              onChange={handleChange}
+            />
+            {cor}
+          </label>
+        );
+      })}
+
+      <ul>
+        {cores.map((cor) => (
+          <li key={cor}>{cor}</li>
+        ))}
+      </ul>
+    </form>
+  );
+};
+
+export default App;
